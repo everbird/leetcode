cafe7f5fde46ca6f5fcef8270dfb0904ca5f1536
Stephen Zhuang <stephen.zhuang@gmail.com>
2013-07-28 18:28:17 +0800

    Solve reverse nodes in k-group

---
+++ ReverseNodeInK-Group.h
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+
+        if (head == NULL ||head->next == NULL || k <=1) {
+            return head;
+        }
+
+        // Get the lenght
+        int length = 0;
+        ListNode *node = head;
+        while (node != NULL) {
+            node = node->next;
+            length++;
+        }
+
+        if (length < k) {
+            return head;
+        }
+
+        int count = k;
+        node = head;
+        ListNode *tail = NULL;
+        ListNode *tmp = NULL;
+        while (count>0) {
+            ListNode *next = node->next;
+            node->next = tmp;
+            tmp = node;
+            node = next;
+            count --;
+        }
+
+        head->next = node;
+        tail = head;
+        head = tmp;
+        tail->next = this->reverseKGroup(tail->next, k);
+        return head;
+    }
+
+};
